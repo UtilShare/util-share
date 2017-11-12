4796137d53cd17f1f142c2f1dac76168c9c4f44d
Devon Herlitz <devonherlitz@gmail.com>
2017-11-11 23:02:12 -0500

    removed test

--- test/utilshare_web/controllers/page_controller_test.exs
+++ test/utilshare_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule UtilshareWeb.PageControllerTest do
   use UtilshareWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
+  #Vue Renders our Pages
+  # test "GET /", %{conn: conn} do
+  #   conn = get conn, "/"
+  #   assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  # end
 end
