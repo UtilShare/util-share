f170e22ed7d3b483a48ca52f5d92523b7b087ee7
Devon Herlitz <devonherlitz@gmail.com>
2017-11-11 22:43:12 -0500

    Vue Works

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,24 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import App from "./components/app.vue"
+import Foo from "./components/foo.vue"
+import Bar from "./components/bar.vue"
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+window.vms = new Vue({
+  router,
+  components: {
+    'app': App
+  }
+}).$mount('#app')
