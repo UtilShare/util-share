dc40e1ec3467e0991251da7a661afacf94fa1bfa
Ben Muschol <benmuschol@gmail.com>
2017-11-14 18:13:45 -0500

    Hello world component

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex';
 import VueRouter from 'vue-router'
+import axios from 'axios';
+
 import App from './components/app.vue'
 import Register from './components/User/register'
 import Login from './components/User/login'
 import CreateHousehold from "./components/Household/create-household";
 import store from './store';
-import axios from 'axios';
+import ExpenseList from './components/Expense/list'
 import types from './store/mutation-types';
 
 Vue.use(VueRouter);
@@ -18,11 +20,13 @@ const routes = [
   { path: '/user/register', component: Register },
   { path: '/user/login', component: Login },
   { path: '/household/create', component: CreateHousehold },
+  { path: '/expenses', component: ExpenseList }
 ]
 
 const router = new VueRouter({
   routes
 })
+
 let config = {
   api: 'http://localhost:4000/api'
 }
---
+++ assets/js/components/Expense/list.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="expenses">
+    Expenses go here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'expense-list'
+}
+</script>
