e39666f91f8bac5df8135921caee28589799bb58
Ben Muschol <benmuschol@gmail.com>
2017-11-14 20:48:10 -0500

    Get a working form

--- assets/js/components/Expense/expense.vue
+++ assets/js/components/Expense/expense.vue
@@ -3,9 +3,30 @@
     <div class="card-body">
       <h4 class="card-title">{{ expense.name }}</h4>
       <h6 class="card-subtitle mb-2 text-muted">{{ expense.household.name }}</h6>
+      <p>Owner: {{ expense.owner.name }}</p>
 
-      <div v-if="isCharging"></div>
-      <a v-else @click.prevent="toggleCharging" class="card-link btn btn-outline-primary">Card link</a>
+      <form class="form-control" @submit.prevent="toggleCharging" v-if="isCharging">
+        <label :for="expense.id + '-amount'">Amount:</label>
+        <input type="number" class="form-control" id="expense.id + '-amount'"/>
+
+        <div class="row expense-split" v-for="user in usersToCharge">
+          <div class="col-md-8">
+            <label :for="expense.id + '-user-' + user.id + '-split'">
+              {{ user.name }}:
+            </label>
+          </div>
+
+          <div class="col-md-4">
+            <input type="number" class="form-control inline"
+              :id="expense.id + '-user-' + user.id + '-split'"/>
+          </div>
+        </div>
+
+        <input type="submit" class="btn btn-primary"/>
+      </form>
+      <a v-else @click.prevent="toggleCharging" class="card-link btn btn-outline-primary">
+        Charge this expense
+      </a>
     </div>
   </div>
 </template>
@@ -14,6 +35,18 @@
 export default {
   'name': 'expense',
   'props': ['expense'],
+
+  data() {
+    return {
+      isCharging: false
+    }
+  },
+
+  computed: {
+    usersToCharge() {
+      return this.expense.household.users.filter(u => u.id != this.expense.owner.id)
+    }
+  },
 
   methods: {
     toggleCharging() {
