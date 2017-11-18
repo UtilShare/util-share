e0a55ed13fa07cf10b0f7cd8c14ffcabe24ab78b
Ben Muschol <benmuschol@gmail.com>
2017-11-18 11:48:14 -0500

    Cleanup household form

--- assets/js/components/Household/create-household.vue
+++ assets/js/components/Household/create-household.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="household">
-      <form @submit.prevent="createHousehold" class="form w-25">
+      <form @submit.prevent="createHousehold" class="form-control">
         <label>Household Name</label>
-        <input v-model="name"/>
+        <input v-model="name" class="form-control"/>
+
+        <label>Users</label>
         <ul>
           <li v-for="email in roommates" v-bind:key="email">{{email}}</li>
         </ul>
+
         <label>Find Users</label>
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="User Email..." v-model="search"/>
-          <span class="input-group-btn">
-            <button class="btn btn-primary" @click.prevent="addMember">+</button>
-          </span>
-        </div>
-        <input type="submit"/>
+        <form @submit.prevent="addMember">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="User Email..." v-model="search"/>
+            <span class="input-group-btn">
+              <input type="submit" class="btn btn-primary" value="+"/>
+            </span>
+          </div>
+        </form>
+        <input type="submit" class="btn btn-primary"/>
       </form>
     </div>
 </template>
