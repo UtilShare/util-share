0eb8abdde8faca37911b2596cf8de72343796a46
Ben Muschol <benmuschol@gmail.com>
2017-11-18 19:03:25 -0500

    Add a 'see more' option for the list

--- assets/js/components/Expense/instance-list.vue
+++ assets/js/components/Expense/instance-list.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="list-group">
-    <li class="list-group-item" :key="instance.id" v-for="instance in instances">
+    <li class="list-group-item" :key="instance.id" v-for="instance in visibleInstances">
       <div class="row">
         <div class="col-3">${{ instance.amount }}</div>
         <div class="col-9">
@@ -8,10 +8,13 @@
         </div>
       </div>
     </li>
+
+    <a href="" v-if="canSeeMore" @click.prevent="seeMore">(See more)</a>
   </ul>
 </template>
 
 <script>
+import _ from 'lodash'
 import InstanceProgress from './instance-progress'
 
 export default {
@@ -19,6 +22,28 @@ export default {
   name: 'expense-list',
   components: {
     'instance-progress': InstanceProgress
+  },
+
+  data() {
+    return {
+      toShow: 3
+    }
+  },
+
+  computed: {
+    visibleInstances() {
+      return _.slice(this.instances, 0, this.toShow)
+    },
+
+    canSeeMore() {
+      return this.toShow < this.instances.length
+    }
+  },
+
+  methods: {
+    seeMore() {
+      this.toShow += 3
+    }
   }
 }
 </script>
