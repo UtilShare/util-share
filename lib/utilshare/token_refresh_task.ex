e4508125291402acb4222c06c5e3f1712b81d703
Devon Herlitz <devonherlitz@gmail.com>
2017-11-21 00:28:06 -0500

    reset timer to 8 min less than the token expirey

--- lib/utilshare/token_refresh_task.ex
+++ lib/utilshare/token_refresh_task.ex
@@ -8,8 +8,8 @@ defmodule Utilshare.TokenRefreshTask do
 
   def run(expires_in) do
     Logger.info "Got token expiring in: " <> Integer.to_string(expires_in)
-    #Sleep for the duration of token less 10 minutes
-    minutes = 59
+    #Sleep for the duration of token less 8 minutes
+    minutes = 8
 
     Logger.info "Sleeping For: " <> Integer.to_string(expires_in-((60*minutes)))
     Process.sleep(1000*(expires_in-((60*minutes))))
